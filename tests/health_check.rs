e217b86f337599505b882b246f23b78b26239167
Suraj Vijay <surajcpp115@gmail.com>
2023-11-28 15:08:43 +0530

    listening to random port for testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -14,8 +14,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length())
 }
 
-async fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() {
+    let server = zero2prod::run("127.0.0.1:0").expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 }
