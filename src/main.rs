226cfc30bd47181c9814c4b7741e48168e45eb0e
Suraj Vijay <surajcpp115@gmail.com>
2023-11-29 20:16:42 +0530

    listening to binded tcp rather than direct binding to hard coded address

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run("127.0.0.1:8000")?.await
+    run(TcpListener::bind("127.0.0.1:8000").expect("Couldn't bind to 8000"))?.await
 }
