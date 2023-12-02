bfc285a43d63ff9a1e7fa77176b30b1c57dbcac8
Suraj Vijay <surajcpp115@gmail.com>
2023-12-02 19:56:15 +0530

    added config crate to read config from custom config file, reorganised lib into modules

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(TcpListener::bind("127.0.0.1:8000").expect("Couldn't bind to 8000"))?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener).expect("Couldn't bind to 8000").await
 }
