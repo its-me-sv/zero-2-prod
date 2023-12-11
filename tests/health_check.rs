583f5886aa167e9027f6ef4ea54730e7f12894dc
Suraj Vijay <surajcpp115@gmail.com>
2023-12-11 11:13:39 +0530

    creating new database with random name for every test run

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
-use sqlx::{Connection, PgConnection, PgPool};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -12,10 +13,10 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
 
     let server =
         zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
@@ -26,6 +27,26 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
 
 #[tokio::test]
@@ -54,7 +75,7 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .expect("Failed to connect Postgres.");
     let client = reqwest::Client::new();
 
-    let body = "name=Suraj%20Vijayan&email=surajvijay67@gmail.com";
+    let body = "name=Suraj%20Vijayan&email=surajvijay67%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
@@ -80,7 +101,7 @@ async fn subscribe_returns_400_when_data_is_missing() {
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=Suraj%20Vijayan", "missing email"),
-        ("email=surajvijay67%40@gmail.com", "missing name"),
+        ("email=surajvijay67%40gmail.com", "missing name"),
         ("", "missing both name and email"),
     ];
 
